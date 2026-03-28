8919bd625eb4870eaddbbaab6045241615321032
oscarboc <oscarboc@hotmail.com>
2026-03-27 20:31:27 -0500

    chore(environment): update default API URL to production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  defaultApiUrl: 'http://localhost:3000'
+  // defaultApiUrl: 'http://localhost:3000'
+  defaultApiUrl: 'https://api-auth.siriscloud.com.co'
 };
