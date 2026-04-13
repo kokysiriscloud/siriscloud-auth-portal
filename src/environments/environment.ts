fa4458d2c26ca7fe9002f1bcfb3dbcc4e4e82eb0
Oscar Bolaños <oscarboc@hotmail.com>
2026-04-12 19:34:51 -0500

    fix: point local auth portal to local auth api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // defaultApiUrl: 'http://localhost:3000'
-  defaultApiUrl: 'https://api-auth.siriscloud.com.co',
+  defaultApiUrl: 'http://localhost:3000',
   metaAppUrl: 'https://national-clam-ghastly.ngrok-free.app/meta/connect',
 };
